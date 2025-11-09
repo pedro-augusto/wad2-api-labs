3dfdde902b456be4aaeb630144beac1785e69a35
pedro-augusto <pedro.canteli2001@gmail.com>
2025-11-09 09:26:43 +0000

    Completed node lab 1!

--- node-lab1/greeting.js
+++ node-lab1/greeting.js
@@ -1,6 +1,9 @@
 const greeting = {
     en: "Hello World!",
-    fr: "Bonjour!"
+    fr: "Bonjour!",
+    ga: "Dia duit!",
+    pt: "Olá, mundo!",
+    es: "Hola, mundo!"
 };
 
 export default greeting;
