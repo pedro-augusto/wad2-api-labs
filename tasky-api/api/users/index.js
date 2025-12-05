be9d48a322d37c671b0e2729e7aa64d8252666f2
pedro-augusto <pedro.canteli2001@gmail.com>
2025-12-05 19:43:49 +0000

    Authentication lab - exercise completed

--- tasky-api/api/users/index.js
+++ tasky-api/api/users/index.js
@@ -31,9 +31,19 @@ router.post('/', asyncHandler(async (req, res) => {
 }));
 
 async function registerUser(req, res) {
-    // Add input validation logic here
-    await User.create(req.body);
-    res.status(201).json({ success: true, msg: 'User successfully created.' });
+  const { password } = req.body;
+
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+  if (!passwordRegex.test(password)) {
+    return res.status(400).json({
+      success: false,
+      msg: 'Password must be at least 8 characters long and include at least one letter, one number, and one special character.',
+    });
+  }
+
+  await User.create(req.body);
+  res.status(201).json({ success: true, msg: 'User successfully created.' });
 }
 
 async function authenticateUser(req, res) {
